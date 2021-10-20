8dc0d862e363a56112329519f613e1761949e96c
emnabz <1947@holbertonschool.com>
2021-10-20 10:54:28 +0100

    task1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,7 +2,7 @@
 """type-annotated function"""
 
 
-def concat(str1: "str", str2: "str"):
+def concat(str1: str, str2: str) -> str:
     """returns a concatenated string"""
 
-    return str1 + "" + str2
+    return str1 + str2
