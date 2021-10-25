87aa3877bb61e44281a7992341faf1e25da14001
emnabz <1947@holbertonschool.com>
2021-10-25 17:24:55 +0100

    task1

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""FIFO caching"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """BasicCache class"""
+
+    def __init__(self):
+        """"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """"""
+        if key is not None and item is not None:
+            self.order.append(key)
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.order.remove(key)
+        else:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                del self.cache_data[self.order[0]]
+                print("DISCARD:", self.order[0])
+                self.order.pop(0)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
