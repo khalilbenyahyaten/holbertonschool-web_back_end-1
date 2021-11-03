996fb31a8712e6d7d3f03093ed69d599f5c7ac56
emnabz <1947@holbertonschool.com>
2021-11-03 23:41:19 +0100

    task5+6

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -9,3 +9,8 @@ def hash_password(password: str) -> bytes:
     """return a hashed, salted password"""
     p = password.encode()
     return bcrypt.hashpw(p, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """returns a boolean"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
