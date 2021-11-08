03936ecbd4a94da775a6f919b1ca5c47aabbeb70
emnabz <1947@holbertonschool.com>
2021-11-08 14:31:45 +0100

    task1

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -20,11 +20,14 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """client request has not been completed
     """
     return jsonify({"error": "Unauthorized"}), 401
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
