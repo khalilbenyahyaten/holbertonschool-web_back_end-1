c883e9d4f70ba4750f8a836ce9076be881541c88
emnabz <1947@holbertonschool.com>
2021-11-08 18:49:54 +0100

    task3

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+create class to manage the API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """defines the Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require auth"""
+        if path and excluded_paths:
+            for ex_path in excluded_paths:
+                if ex_path.endswith('/') or ex_path.endswith('*'):
+                    ex_path = ex_path[:-1]
+                if ex_path in path:
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header"""
+        if request:
+            return request.headers.get('Authorization')
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Flask request object"""
+        return None
