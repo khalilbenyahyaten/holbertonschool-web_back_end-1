313dac0604e1d4b089b97f3c4cab41ff6a081449
emnabz <1947@holbertonschool.com>
2021-11-10 18:25:15 +0100

    task8

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -34,3 +34,19 @@ class BasicAuth(Auth):
             return string_bytes.decode('utf-8')
         except Exception:
             return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """decode Base64 Authorization Header """
+        if (
+            not base64_authorization_header
+            or not not isinstance(base64_authorization_header, str)
+        ):
+            return None
+            try:
+                b64_bytes = base64_authorization_header.encode('utf-8')
+                string_bytes = base64.b64decode(b64_bytes)
+                return string_bytes.decode('utf-8')
+            except Exception:
+                return None
