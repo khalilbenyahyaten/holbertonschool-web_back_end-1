4192ad63e012f231a8a26237cf497c709110fb19
emnabz <1947@holbertonschool.com>
2021-11-16 23:58:31 +0100

    task0

--- 0x07-Session_authentication/api/v1/views/users.py
+++ 0x07-Session_authentication/api/v1/views/users.py
@@ -33,6 +33,16 @@ def view_one_user(user_id: str = None) -> str:
     return jsonify(user.to_json())
 
 
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def me() -> str:
+    """ GET /api/v1/users/me
+    Return:
+      - User object JSON represented
+      - 404 if the User ID doesn't exist
+    """
+    return jsonify(request.current_user.to_json())
+
+
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
     """ DELETE /api/v1/users/:id
