f9c419bcada2ec6986b2153270fbab4701bd6a3a
emnabz <1947@holbertonschool.com>
2021-11-19 17:54:45 +0100

    task6

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def idex():
+    """JSON form"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
