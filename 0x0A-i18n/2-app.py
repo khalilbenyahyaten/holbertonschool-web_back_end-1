f93531087ea23ffa7b21ae83b3ffcbe9ece344c9
emnabz <1947@holbertonschool.com>
2021-11-28 19:48:58 +0100

    task2

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -7,15 +7,18 @@ import babel
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 class Config():
     """ Configuration for babel translation """
     LANGUAGES = ["en", "fr"]
+
 
 app = Flask(__name__)
 app.config.from_object(Config())
 Babel.default_locale = "en"
 Babel.default_timezone = "UTC"
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale():
