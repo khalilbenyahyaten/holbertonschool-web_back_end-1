80cf3eb0ba08947cbcfe2ff5589dfd39927abdfa
emnabz <1947@holbertonschool.com>
2021-11-28 17:07:55 +0100

    task6

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -28,3 +28,17 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient('abc')
         result = client._public_repos_url
         self.assertEqual(result, "test_value")
+
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, patch):
+        """ Tests '_public_repos_url' class method that
+        has been turned into a property by 'memoize' """
+        patch.return_value = {"repos_url": "test_value"}
+        client = GithubOrgClient('abc')
+        result = client._public_repos_url
+        self.assertEqual(result, "test_value")
+
+    @parameterized.expand([({"license": {"key": "my_license"}},
+                            "my_license", True),
+                           ({"license": {"key": "other_license"}},
+                            "my_license", False)])
