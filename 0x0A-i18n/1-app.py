742ab46d738c5c3e408e31d906aab8e98a9cb21a
emnabz <1947@holbertonschool.com>
2021-11-28 19:31:09 +0100

    task1

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config():
+    """config lang"""
+    LANGUAGES = ["en", "fr"]
+
+app = Flask(__name__)
+app.config.from_object(Config())
+Babel.default_locale = "en"
+Babel.default_timezone = "UTC"
+babel = Babel(app)
