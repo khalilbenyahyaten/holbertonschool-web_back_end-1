3fc0bfc06118d5327be90b454aa6201699713ad0
emnabz <1947@holbertonschool.com>
2022-01-14 15:30:09 +0100

    task7

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -2,21 +2,30 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-
-app.get('/students', async (req, res) => {
-  countStudents(process.argv[2])
-    .then((message) => {
-      const response = `This is the list of our students\n${message}`;
-      res.send(response);
-    })
-    .catch((err) => {
-      res.send(`${err.message}`);
-    });
-});
+const port = 1245;
+const database = process.argv[2];
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(1245);
+app.get('/students', (req, res) => {
+  countStudents(database)
+    .then((data) => {
+      const { students, csStudents, sweStudents } = data;
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${students.length}\n`);
+      res.write(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+      res.statusCode = 200;
+      res.end();
+    })
+    .catch((error) => {
+      res.statusCode = 500;
+      res.send(`This is the list of our students\n${error.message}`);
+    });
+});
+
+app.listen(port);
+
 module.exports = app;
