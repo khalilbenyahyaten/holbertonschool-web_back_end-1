96cdecd0dbc7e0e0574dc9082075699cf28cd365
emnabz <1947@holbertonschool.com>
2022-01-19 15:40:08 +0100

    task11

---
+++ 0x0C-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- dummy comment dummy comment dummy comment
+-- dummy comment dummy comment dummy comment
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+SELECT name FROM students
+WHERE score < 80
+AND ISNULL(last_meeting)
+OR last_meeting < ADDDATE(CURDATE(), INTERVAL -1 MONTH);
